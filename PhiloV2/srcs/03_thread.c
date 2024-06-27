2ab4efe7bccc16c29590eeefdbd3a9330be76c73
cabarbaye <cabarbaye.adrien@gmail.com>
2024-06-27 15:26:59 +0200

    refacto routine

--- PhiloV2/srcs/03_thread.c
+++ PhiloV2/srcs/03_thread.c
@@ -6,7 +6,7 @@
 /*   By: acabarba <acabarba@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/23 17:22:02 by acabarba          #+#    #+#             */
-/*   Updated: 2024/06/27 15:06:33 by acabarba         ###   ########.fr       */
+/*   Updated: 2024/06/27 15:22:08 by acabarba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,79 +42,81 @@ void	*routine(void *arg)
 	philo = (t_philo *) arg;
 	while (1)
 	{
-		// Gestion de la Mort, si attente depuis le dernier repas > philo->data->time_to_death
-		if (philo->data->someone_died)
-			break ;
+		// Vérification de la mort et de l'état général
 		gettimeofday(&current_time, NULL);
 		time_diff = (current_time.tv_sec - philo->last_meal.tv_sec) * 1000 + 
 		            (current_time.tv_usec - philo->last_meal.tv_usec) / 1000;
-		if (time_diff > philo->data->time_to_die)
+		if (time_diff > philo->data->time_to_die || philo->data->someone_died)
 		{
 			pthread_mutex_lock(&philo->data->printex);
-			ft_printf("Philosophe n° %d is dead.\n", philo->id);
+			if (time_diff > philo->data->time_to_die)
+			{
+				ft_printf("Philosophe n° %d is dead.\n", philo->id);
+				philo->data->someone_died = 1;
+			}
 			pthread_mutex_unlock(&philo->data->printex);
-			philo->data->someone_died = 1;
 			break;
 		}
-		//
-
-		if (philo->data->someone_died)
-			break ;
 
 		// Penser
 		pthread_mutex_lock(&philo->data->printex);
 		ft_printf("\033[33mPhilosophe n°\33[0m %d \033[33mthinking.\033[0m\n", philo->id);
 		pthread_mutex_unlock(&philo->data->printex);
-		//
-
-		if (philo->data->someone_died)
-			break ;		
 
 		// Prendre les fourchettes
 		pthread_mutex_lock(philo->right_fork);
 		pthread_mutex_lock(&philo->data->printex);
 		ft_printf("\033[31mPhilosophe n°\33[0m %d \033[31mtake RIGHT_Fork.\033[0m\n", philo->id);
 		pthread_mutex_unlock(&philo->data->printex);
+
 		pthread_mutex_lock(philo->left_fork);
 		pthread_mutex_lock(&philo->data->printex);
 		ft_printf("\033[31mPhilosophe n°\33[0m %d \033[31mtake LEFT_Fork.\033[0m\n", philo->id);
 		pthread_mutex_unlock(&philo->data->printex);
-		//
-		if (philo->data->someone_died)
+
+		// Vérification de la mort avant de manger
+		gettimeofday(&current_time, NULL);
+		time_diff = (current_time.tv_sec - philo->last_meal.tv_sec) * 1000 + 
+		            (current_time.tv_usec - philo->last_meal.tv_usec) / 1000;
+		if (time_diff > philo->data->time_to_die || philo->data->someone_died)
 		{
 			pthread_mutex_unlock(philo->right_fork);
 			pthread_mutex_unlock(philo->left_fork);
-			break ;
-		}
-		// Manger (sans gestion des forks avec gestion du nombre de repas)
-		if (philo->data->nb_meal_needed == -1 || philo->nb_meal < philo->data->nb_meal_needed)
-		{
 			pthread_mutex_lock(&philo->data->printex);
-			ft_printf("\33[1;34mPhilosophe n°\33[0m %d \33[1;34mstart eating.\n\33[0m", philo->id);
+			if (time_diff > philo->data->time_to_die)
+			{
+				ft_printf("Philosophe n° %d is dead.\n", philo->id);
+				philo->data->someone_died = 1;
+			}
 			pthread_mutex_unlock(&philo->data->printex);
-			gettimeofday(&philo->last_meal, NULL); // Mettre à jour l'horodatage du dernier repas
-			usleep(1000 * philo->data->time_to_eat);
-			philo->nb_meal++;
-			pthread_mutex_lock(&philo->data->printex);
-			ft_printf("\33[3;34mPhilosophe n°\33[0m %d \33[3;34mfinished eating.\n\33[0m", philo->id);
-			pthread_mutex_unlock(&philo->data->printex);
+			break;
 		}
-		if (philo->data->someone_died)
-		{
-			pthread_mutex_unlock(philo->right_fork);
-			pthread_mutex_unlock(philo->left_fork);
-			break ;
-		}	
-		// Lache la fourchette
+
+		// Manger
+		pthread_mutex_lock(&philo->data->printex);
+		ft_printf("\33[1;34mPhilosophe n°\33[0m %d \33[1;34mstart eating.\n\33[0m", philo->id);
+		pthread_mutex_unlock(&philo->data->printex);
+
+		gettimeofday(&philo->last_meal, NULL); // Mettre à jour l'horodatage du dernier repas
+		usleep(1000 * philo->data->time_to_eat);
+		philo->nb_meal++;
+
+		pthread_mutex_lock(&philo->data->printex);
+		ft_printf("\33[3;34mPhilosophe n°\33[0m %d \33[3;34mfinished eating.\n\33[0m", philo->id);
+		pthread_mutex_unlock(&philo->data->printex);
+
+		// Lâcher les fourchettes
 		pthread_mutex_unlock(philo->right_fork);
 		pthread_mutex_lock(&philo->data->printex);
 		ft_printf("\033[31mPhilosophe n°\33[0m %d \033[31mdrop RIGHT_Fork.\033[0m\n", philo->id);
 		pthread_mutex_unlock(&philo->data->printex);
+
 		pthread_mutex_unlock(philo->left_fork);
 		pthread_mutex_lock(&philo->data->printex);
 		ft_printf("\033[31mPhilosophe n°\33[0m %d \033[31mdrop LEFT_Fork.\033[0m\n", philo->id);
 		pthread_mutex_unlock(&philo->data->printex);
-		//
+
+		// Vérifier si le philosophe a mangé assez de fois
 		if (philo->data->nb_meal_needed > 0 && philo->nb_meal == philo->data->nb_meal_needed)
 		{
 			pthread_mutex_lock(&philo->data->printex);
@@ -122,18 +124,13 @@ void	*routine(void *arg)
 			pthread_mutex_unlock(&philo->data->printex);
 			break;
 		}
-		//
-		if (philo->data->someone_died)
-			break ;
+
 		// Dormir
 		pthread_mutex_lock(&philo->data->printex);
 		ft_printf("\033[32mPhilosophe n°\33[0m %d \033[32mgo to bed.\033[0m\n", philo->id);
-		usleep(1000 * philo->data->time_to_sleep);
 		pthread_mutex_unlock(&philo->data->printex);
-		//
-		if (philo->data->someone_died)
-			break ;
-		
+		usleep(1000 * philo->data->time_to_sleep);
 	}
 	return (NULL);
 }
+
