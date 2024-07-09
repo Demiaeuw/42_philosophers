5d585f46c4cfc2b076be44583629fbecd02ee65a
cabarbaye <cabarbaye.adrien@gmail.com>
2024-07-09 21:28:53 +0200

    tester ok normalement

--- PhiloV2/test/check_valgrind.py
+++ PhiloV2/test/check_valgrind.py
@@ -10,7 +10,14 @@ command = sys.argv[1:]
 process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 stdout, stderr = process.communicate()
 
-if b"definitely lost: 0 bytes" in stderr and b"indirectly lost: 0 bytes" in stderr:
+# Afficher la sortie standard et la sortie d'erreur pour le débogage
+print("STDOUT:")
+print(stdout.decode())
+print("STDERR:")
+print(stderr.decode())
+
+# Vérifier les fuites de mémoire dans la sortie de Valgrind
+if b"All heap blocks were freed -- no leaks are possible" in stderr:
     print("Memory Leak Test Passed")
     sys.exit(0)
 else:
